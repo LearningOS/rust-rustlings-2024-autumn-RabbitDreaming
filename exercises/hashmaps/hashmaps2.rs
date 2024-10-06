101d2d564cb436c1951d8b3d698313f20c8a3d19
hlh <1759718950@qq.com>
2024-10-06 23:35:56 +0800

    dosome

--- exercises/hashmaps/hashmaps2.rs
+++ exercises/hashmaps/hashmaps2.rs
@@ -14,7 +14,7 @@
 // Execute `rustlings hint hashmaps2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 use std::collections::HashMap;
 
@@ -40,6 +40,13 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         // TODO: Insert new fruits if they are not already present in the
         // basket. Note that you are not allowed to put any type of fruit that's
         // already present!
+        if !basket.contains_key(&fruit) {  
+            match fruit {  
+                Fruit::Banana => basket.insert(fruit, 1), // 添加一个香蕉  
+                Fruit::Pineapple => basket.insert(fruit, 4), // 添加四个菠萝  
+                _ => None, // 其他水果不添加  
+            };  
+        }  
     }
 }
 
